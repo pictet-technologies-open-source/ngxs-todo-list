2077399c0305230cf9928b836a87811c90d4ee4d
Maureen Heitzmann <mheitzmann@pictet-technologies.com>
2021-09-22 14:56:58 +0200

    organize imports

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './components/app.component';
 import {FormsModule} from '@angular/forms';
 import {NgxsModule} from '@ngxs/store';
 import {environment} from '../environments/environment';
